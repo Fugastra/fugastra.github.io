ecd2ec3a2e46663559d8cde5613a1f82e0cffabc
Fugastra <argosiathai@gmail.com>
2026-01-08 22:38:11 +0800

    added Js

---
+++ script.js
@@ -0,0 +1,7 @@
+function share() {
+   // Copy the text inside the text field
+  navigator.clipboard.writeText("https://fugastra.github.io/");
+
+  // Alert the copied text
+  alert("Successfully copied link! Thank you for sharing :)) | 成功複製，感謝分享");
+} 
